137867fbfd78fd1798f2794cc818a24f4a20623e
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-15 22:55:43 +0530

    Program for Storing 10 random numbers into a array

--- ArrayPrac.js
+++ ArrayPrac.js
@@ -1,9 +1,11 @@
 console.log("Wellcome to the Array Practice Problems ");
 
-
+const ARRAY_LENGTH = 10
+const randomArray = []
 for (var i = 1; i <= 10; i++) {
-    let x = Math.floor(Math.random()*(999-100+1)+100);
-     console.log("Three digit number "+ i+" = "+ x);
-    }
-    
+   let x = Math.floor(Math.random()*(999-100+1)+100);
+    randomArray.splice(i,0,x)
+   }
+   console.log(randomArray);
+
  
