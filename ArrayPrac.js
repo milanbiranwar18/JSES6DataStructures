a31a7886f6aee2b30e325e963634dab9a16faeaa
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-15 22:49:13 +0530

    Program for generating 10 random 3 digit number

--- ArrayPrac.js
+++ ArrayPrac.js
@@ -1,1 +1,9 @@
 console.log("Wellcome to the Array Practice Problems ");
+
+
+for (var i = 1; i <= 10; i++) {
+    let x = Math.floor(Math.random()*(999-100+1)+100);
+     console.log("Three digit number "+ i+" = "+ x);
+    }
+    
+ 
